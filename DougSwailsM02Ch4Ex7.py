4f32b07552ada6ebc8ed846415feb7a2f96cef43
dougs1410 <56087206+dougs1410@users.noreply.github.com>
2019-11-07 23:04:40 -0500

    Add files via upload

---
+++ DougSwailsM02Ch4Ex7.py
@@ -0,0 +1,30 @@
+#Pennies
+#doug swails
+#version 1.1
+#m02Ch4Ex7
+
+#user input
+days=int(input('How many days did you earn? '))
+
+#display
+print('\nDays\tPennies')
+print('---------------')
+
+#Loop
+for num in range(1,days+1):
+    pennies=num+1
+    print(num,'\t',pennies)
+    
+    
+        
+
+    
+    
+  
+            
+        
+
+        
+           
+    
+
